b765263d2e9ffa371f1855e1ed3225f6ebb9e78a
Casey Jardin <casey@blackairplane.com>
2019-10-25 22:43:02 -0400

    hide background video caption on mobile devices

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -17,8 +17,8 @@ export default class ContactUs extends Component {
               I’m open to full-time Front-End Engineering opportunities with a company who wants to improve the world!
               I am also available for freelance work. Click the icons below to get in touch!
             </p>
-            <div class="row">
-              <small>Like the video above? I helped create the app, and more!</small>
+            <div class="video-caption">
+              <small>Like the background video? I helped create the app, and more!</small>
               <small><i className="fas fa-link" /> <a href="https://www.mbusa.com/mercedesme/" rel="noopener noreferrer" target="_blank">mbusa.com</a> | Mercedes-Benz USA, LLC, Senior Developer</small>
             </div>
           </div>
